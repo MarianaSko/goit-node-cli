f465b0517484a5515770a8ee55c3889e53151d75
MarianaSko <maryana.skorynovych@gmail.com>
2024-02-02 18:59:11 +0200

    tasks done v-1

---
+++ index.js
@@ -0,0 +1,41 @@
+import { program } from "commander";
+import * as contactService from "./src/contacts.js"
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case "list":
+            const allContacts = await contactService.getAllContacts();
+            console.log(allContacts);
+            break;
+
+        case "get":
+            const contact = await contactService.getContactById(id);
+            console.log(contact)
+            break;
+
+        case "add":
+            const newContact = await contactService.addContact(name, email, phone)
+            console.log(newContact);
+            break;
+
+        case "remove":
+            const deletedContact = await contactService.removeContact(id);
+            console.log(deletedContact);
+            break;
+
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+invokeAction(options);
